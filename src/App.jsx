f383b5ad148bfe1582867a71a6ff20e7985995d2
Carlos Pires <extraterra1@gmail.com>
2023-11-10 13:45:24 +0000

    dev: add webfontloader + iconify

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import Header from './Components/Header';
+
 function App() {
-  return <h1>Hey</h1>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import WebFont from 'webfontloader';
+import './index.css';
+
+WebFont.load({
+  google: {
+    families: ['Open Sans:300,400,700']
+  }
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
