1710e5eec8c7480c26fcb6198e5fff2f087f1ce8
Carlos Pires <extraterra1@gmail.com>
2023-11-14 11:17:49 +0000

    style: basic shop page styling

--- src/Components/Shop.jsx
+++ src/Components/Shop.jsx
@@ -1,11 +1,66 @@
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import Header from './Header';
+
+const MainContainer = styled.main`
+  display: flex;
+  flex-direction: column;
+
+  & > section {
+    padding: 5rem;
+    display: flex;
+  }
+
+  @media screen and (max-width: 700px) {
+    & > section {
+      padding: 3rem;
+    }
+  }
+`;
+
+const ShopSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  background-color: #5c5c5c;
+
+  & .title {
+    font-family: 'Chakra Petch';
+    & h1 {
+      font-size: 6rem;
+    }
+    & p {
+      font-size: 2rem;
+    }
+  }
+`;
+
+const ProductsGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(50rem, 1fr));
+  justify-items: center;
+
+  margin-top: 5rem;
+`;
+
 const Shop = ({ cart, products, setCart, error }) => {
   return (
     <>
       <Header cart={cart} />
-      <h1>Hi</h1>
-      <h1>Hi</h1>
+      <MainContainer>
+        <ShopSection>
+          <div className="title">
+            <h1>Explore the PixelPerfection</h1>
+            <p>Uncover a Symphony of Digital Innovation in Our Exclusive Product Collection!</p>
+          </div>
+          <ProductsGrid>
+            <h2>Card</h2>
+            <h2>Card</h2>
+            <h2>Card</h2>
+            <h2>Card</h2>
+            <h2>Card</h2>
+          </ProductsGrid>
+        </ShopSection>
+      </MainContainer>
     </>
   );
 };
