67d6e0f6fdb4545f324c702cc2bb3aa40189f699
Carlos Pires <extraterra1@gmail.com>
2023-11-14 12:36:00 +0000

    feat: build star ratings based off number

--- src/Components/StarRating.jsx
+++ src/Components/StarRating.jsx
@@ -1,16 +1,22 @@
 import { Icon } from '@iconify/react';
 import PropTypes from 'prop-types';
+import { v4 as uuid } from 'uuid';
 
 const StarRating = ({ rating }) => {
-  return (
-    <>
-      <Icon icon="ion:ios-star" />
-      <Icon icon="ion:ios-star" />
-      <Icon icon="ion:ios-star" />
-      <Icon icon="ion:ios-star" />
-      <Icon icon="ion:ios-star" />
-    </>
-  );
+  const int = parseInt(rating);
+  const float = rating % 1;
+  const stars = [];
+  for (let i = 0; i < int; i++) {
+    stars.push(<Icon key={uuid()} icon="ion:ios-star" />);
+  }
+  if (float >= 0.5) stars.push(<Icon key={uuid()} icon="ion:ios-star-half" />);
+  if (stars.length < 5) {
+    const starsToAdd = 5 - stars.length;
+    for (let i = 0; i < starsToAdd; i++) {
+      stars.push(<Icon key={uuid()} icon="ion:ios-star-outline" />);
+    }
+  }
+  return <>{stars}</>;
 };
 
 StarRating.propTypes = {
