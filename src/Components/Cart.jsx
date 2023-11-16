3c4d88a6ef05c8ad37ad6944ed71b1163b350c86
Carlos Pires <extraterra1@gmail.com>
2023-11-16 12:05:53 +0000

    style: basic cart styling

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,13 +1,54 @@
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import Header from './Header';
 import Footer from './Footer';
+
+const MainContainer = styled.main`
+  display: flex;
+  flex-direction: column;
+
+  & > section {
+    padding: 5rem;
+    display: flex;
+  }
+
+  @media screen and (max-width: 700px) {
+    & > section {
+      padding: 3rem;
+    }
+  }
+`;
+
+const CartSection = styled.section`
+  background-color: #5c5c5c;
+  flex-direction: column;
+  gap: 5rem;
+
+  & h1 {
+    font-size: 4rem;
+    font-family: 'Chakra Petch';
+    letter-spacing: 1px;
+  }
+`;
+
+const CartGrid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  border: 2px dashed #e3e3e3;
+  padding: 3rem;
+`;
 
 const Cart = ({ cart }) => {
   return (
     <>
       <Header cart={cart} />
-      <h1>Hey</h1>
-      <Footer />
+      <MainContainer>
+        <CartSection>
+          <h1 className="title">Your Cart</h1>
+          <CartGrid>Item Item</CartGrid>
+        </CartSection>
+      </MainContainer>
+      {/* <Footer /> */}
     </>
   );
 };
