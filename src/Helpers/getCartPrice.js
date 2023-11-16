31a1f5bd8dad0be0227272ab9c6d7e28ba471c59
Carlos Pires <extraterra1@gmail.com>
2023-11-16 15:20:43 +0000

    fix: getCartPrice not rounding correctly

--- src/Helpers/getCartPrice.js
+++ src/Helpers/getCartPrice.js
@@ -3,7 +3,7 @@ const getCartPrice = (cart) => {
   const totalPrice = cart.reduce((acc, el) => acc + el.quantity * el.product.price, 0);
   const subTotal = +(totalPrice / (1 + vatRate)).toFixed(2);
   const vat = +(subTotal * vatRate).toFixed(2);
-  return { total: subTotal + vat, vat, subTotal };
+  return { total: (subTotal + vat).toFixed(2), vat, subTotal };
 };
 
 export default getCartPrice;
